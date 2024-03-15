c4920892778fb9604b59c1d25cba7f83cdf54bd9
Vedavit Shetty <vedavit@berkeley.edu>
2024-03-15 01:37:00 -0700

    register models on admin

--- backend/recipe_organizer/admin.py
+++ backend/recipe_organizer/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 
-from .models import Recipe, Restaurant
+from .models import Employee, Recipe, Restaurant
 
 
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ("title", "source_url")
+    list_display = ("title", "ingredients", "source_url")
 
 
 class RestaurantAdmin(admin.ModelAdmin):
-    list_display = ("name", "address")
+    list_display = ("name", "owner")
 
 
 # Register your models here.
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Employee)
