e98676c20a7d32a5142000c8967a5285688ad0f0
Vedavit Shetty <vedavit@berkeley.edu>
2024-03-19 00:22:30 -0700

    show NotFoundPage for recipes that don't exist

--- frontend/js/pages/ViewRecipePage.js
+++ frontend/js/pages/ViewRecipePage.js
@@ -4,35 +4,45 @@ import { useParams } from 'react-router-dom';
 import { fetchRecipeById } from '../store/recipeSlice';
 import { parseIngredients } from '../constants';
 import LogoutButton from '../components/LogoutButton';
+import NotFoundPage from './NotFoundPage'; // Import NotFoundPage component
 
 const ViewRecipePage = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const recipe = useSelector((state) => state.recipeSlice.currentRecipe);
+  const loading = useSelector((state) => state.recipeSlice.loading);
 
   useEffect(() => {
     dispatch(fetchRecipeById(id));
   }, [dispatch, id]);
 
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!recipe) {
+    return <NotFoundPage />;
+  }
+
+  if (Object.keys(recipe).length === 0) {
+    return <NotFoundPage />;
+  }
+
   return (
     <div className='mt-3'>
       <LogoutButton />
       <h2>Recipe Details</h2>
-      {recipe ? (
-        <div>
-          <h3>{recipe.title}</h3>
-          <p>Ingredients:</p>
-          <ul>
-            {parseIngredients(recipe.ingredients).map((ingredient, index) => (
-              <li key={index}>{ingredient}</li>
-            ))}
-          </ul>
-          <p>Instructions:</p>
-          <p>{recipe.instructions}</p>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <div>
+        <h3>{recipe.title}</h3>
+        <p>Ingredients:</p>
+        <ul>
+          {parseIngredients(recipe.ingredients).map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+          ))}
+        </ul>
+        <p>Instructions:</p>
+        <p>{recipe.instructions}</p>
+      </div>
     </div>
   );
 };
